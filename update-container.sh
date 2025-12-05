8a085bf3c8bc38fa59182803f7364a8270dff3ed
Josh Espinoza <joshespi@gmail.com>
2025-12-05 13:30:08 -0700

    correct command

--- update-container.sh
+++ update-container.sh
@@ -7,7 +7,7 @@ docker pull phpmyadmin:latest
 # Rebuild and restart the container
 echo "Rebuilding and restarting the phpMyAdmin container..."
 cd /docker/host-myadmin
-docker-compose down
-docker-compose up -d
+docker compose down
+docker compose up -d
 
 echo "Update complete!"
